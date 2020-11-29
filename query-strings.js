f47a3783a86a9f67a89aba350cca6eaec6354fac
Shiinobu <akarapong024@gmail.com>
2020-11-29 18:57:42 +0700

    Updated set language.

--- query-strings.js
+++ query-strings.js
@@ -1,7 +1,7 @@
 // Library: query string static path
 // Description: for get query string from html path.
 // Author: akarapongdev
-// Version: 1.0
+// Version: 1.1
 // Licenses: copyright hibizsfot devloper team
 
 class QS {
@@ -9,22 +9,36 @@ class QS {
     this.params = new URLSearchParams(window.location.search);
     this.path = '';
   }
-  setUTM = function (path = '') {
-    let utm_object = {
-      'utm_medium': '',
-      'utm_source': '',
-      'utm_campaign': ''
-    };
+  setUTM = function (path = '', lang = '') {
+    let utm_object = { 'utm_medium': '', 'utm_source': '', 'utm_campaign': ''};
     let params_path = '';
     let prefix = '&';
+    let lang_params = '';
     for (let key in utm_object) {
       if (key === 'utm_medium') {
-        prefix = '?';
+        prefix = '';
       } else {
         prefix = '&';
       }
+      if (lang === '') {
+        lang_params = '';
+      } else {
+        lang_params = 'lang=' + lang + '&';
+      }
       params_path += prefix + key + '=' + this.params.get(key) || '';
     }
-    return this.path = path + params_path;
+    return this.path = path + '?' + lang_params + params_path;
   };
+
+  setLanguage = function (path, lang = '') {
+    if (!path) return false;
+    this.path = path;
+    let lang_params = '';
+    if (lang === '') {
+      lang_params = '';
+    } else {
+      lang_params = 'lang=' + lang;
+    }
+    return this.path + '?' + lang_params;
+  }
 }
